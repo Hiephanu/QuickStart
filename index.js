e4e723103ff7934016ef5fe2039621bdf19c15c4
Hiephanu <doanvanhiep230903@gmail.com>
2024-06-29 14:44:16 +0700

    update code

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const { SourceTextModule } = require('vm')
 const program = new Command()
 
 program.version("0.0.1").description("A cli to open application when type name of aplication")
+const huhu = ()=> {
+  console.log("haha");
+}
 
 const getInstalledApplication = (callback) => {
     const command = `reg query "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" /s /v "DisplayName"`
